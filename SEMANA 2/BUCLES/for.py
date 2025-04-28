e906daa2f0efa8858ce92b5376b2bf473c3a0a0b
Mayerly Zapata <mayerlyzapatarodriguez@gmail.com>
2025-04-28 13:49:05 -0500

    bucles

---
+++ SEMANA 2/BUCLES/for.py
@@ -0,0 +1,15 @@
+nombre="MAYERLY ZAPATA"
+for i in range (3):
+    print(nombre)
+
+for x in "Banana":
+    print(x)
+
+#SE ROMPE CUANDO LLEGA A LA POSICION BANANO
+frutas=["uva", "pera", "banano", "mango"]
+for x in "banano":
+    print(x)
+    if x =="banano":
+        break
+
+    
